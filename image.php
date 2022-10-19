0c5d13eae1691a73dc3b753d5442935a59618d80
Alex <alexandre.autret@gmail.com>
2022-10-19 18:02:46 +0200

    first commit

---
+++ image.php
@@ -0,0 +1,34 @@
+<?php
+$domain = $_GET['domain'];
+$token  = intval($_GET['tokenId']);
+
+
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+$dest = imagecreatefromjpeg('bg.jpg');
+
+$color = imagecolorallocate($dest, 255, 255, 255);
+$font_path = 'TwCenClassMTStd-Regular.ttf';
+$string = $domain.".ethereum";
+$length = strlen($string) ;
+if($length < 15){
+    $fontSize= 80;
+} elseif($length < 20){
+    $fontSize = 50;
+} else {
+    $fontSize = 40;
+}
+$image_width= 1080;
+$image_height= 1080;
+$text_box = imagettfbbox($fontSize,0,$font_path,$string);
+
+$text_width = $text_box[2]-$text_box[0];
+$text_height = $text_box[7]-$text_box[1];
+
+$x = ($image_width/2) - ($text_width/2);
+
+imagettftext($dest, $fontSize, 0, $x, 900, $color, $font_path, $string);
+header('Content-Type: image/jpg');
+imagejpeg($dest);
+imagedestroy($dest);
+
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+$domain = $_GET['domain'];
+$token  = intval($_GET['tokenId']);
+
+if($token <= 999) {
+    $club = '999 Club';
+} elseif($token <= 10000){
+    $club = '10k Club';
+} elseif($token <= 100000) {
+    $club = '100k Club';
+} else {
+    $club = false;
+}
+
+
+if(is_numeric($domain)) {
+    $type = 'Digits';
+} elseif (!preg_match('/[0-9]/', $domain)) {
+    $type = 'Letters';
+} else {
+    $type = 'Mixed';
+}
+
+$metaData = [
+    "name" =>  $domain,
+
+  "description" => $domain . ".ethereum, an ethereum name.",
+  "attributes" =>  [
+    [
+        "trait_type"=> "Character Set",
+      "display_type"=>  "string",
+      "value"=> $type // If MYNAME is only Alphabets then: "Letters", If AlphaNumerical: "Mixed", If Numbers only: "Digits"
+    ],
+  ],
+    "name_length" => strlen($domain), // length of MYNAME
+    "url"=> $_SERVER['SCRIPT_URI'],
+    "image_url" => str_replace('/'.$token.'/' . $domain , "/images/" .$token.'/' . $domain ,$_SERVER['SCRIPT_URI']) ,
+];
+if($club){
+    $metaData['attributes'][] = [
+        "trait_type"=> "Club",
+        "display_type"=> "string",
+        "value"=> $club  // Check saved JSONs so far, If saved < 999: '999 Club', else if saved < 10000: '10K Club', else if saved < 10000: '100K Club'
+    ];
+}
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($metaData);
+exit();
